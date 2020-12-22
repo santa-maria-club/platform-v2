e8b69add66829011952b91a8b0d5233e48fd55b5
Daniel Marin <danielarturomt@gmail.com>
2020-12-22 14:55:44 -0300

    unit: set up initial testing for create in graph controller

--- apps/irrigation-api/src/app/graph/graph.controller.spec.ts
+++ apps/irrigation-api/src/app/graph/graph.controller.spec.ts
@@ -1,18 +1,43 @@
 import { Test, TestingModule } from '@nestjs/testing';
+
 import { GraphController } from './graph.controller';
+import { GraphServiceMock, GRAPH_SERVICE } from './graph.service';
+import type { IGraphService } from './graph.service';
 
 describe('GraphController', () => {
   let controller: GraphController;
+  let graphService: IGraphService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [GraphController],
+      providers: [
+        {
+          provide: GRAPH_SERVICE,
+          useClass: GraphServiceMock,
+        },
+      ],
     }).compile();
 
     controller = module.get<GraphController>(GraphController);
+    graphService = module.get<IGraphService>(GRAPH_SERVICE);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('Create Method', () => {
+    it('should call create from graph service', () => {
+      // arrange
+      const createGraphDto = { name: 'test' };
+      const createSpy = jest.spyOn(graphService, 'create');
+      const options = { rootDirectory: __dirname };
+      // act
+      controller.create(createGraphDto).subscribe();
+      // assert
+      expect(createSpy).toHaveBeenCalled();
+      expect(createSpy).toHaveBeenCalledWith(createGraphDto, options);
+    });
   });
 });
