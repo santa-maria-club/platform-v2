e7d0d877ec9c455b1be23bb3baf384a54d6f2d53
Daniel Marin <danielarturomt@gmail.com>
2020-12-22 13:45:38 -0300

    unit: set up testing for file manager

--- libs/shared/utils/file-manager/src/lib/shared-utils-file-manager.service.spec.ts
+++ libs/shared/utils/file-manager/src/lib/shared-utils-file-manager.service.spec.ts
@@ -1,4 +1,5 @@
 import { Test } from '@nestjs/testing';
+
 import { SharedUtilsFileManagerService } from './shared-utils-file-manager.service';
 
 describe('SharedUtilsFileManagerService', () => {
@@ -14,5 +15,34 @@ describe('SharedUtilsFileManagerService', () => {
 
   it('should be defined', () => {
     expect(service).toBeTruthy();
+  });
+
+  describe('Write File Method', () => {
+    it('should save the file in assets/graphs/<name>.json', () => {
+      // arrange
+      const name = 'test';
+      const path = `${__dirname}/assets/graphs/${name}.json`;
+      const writeFileSpy = jest.spyOn(service.fs, 'writeFile');
+      const contentAsString = '{}';
+      // act
+      service.writeFile(name, {}).subscribe();
+      // assert
+      expect(writeFileSpy).toHaveBeenCalled();
+      expect(writeFileSpy).toHaveBeenCalledWith(path, contentAsString);
+    });
+
+    it('should store content as a JSON string', () => {
+      // arrange
+      const name = 'test';
+      const path = `${__dirname}/assets/graphs/${name}.json`;
+      const writeFileSpy = jest.spyOn(service.fs, 'writeFile');
+      const content = { name };
+      const contentAsString = JSON.stringify(content);
+      // act
+      service.writeFile(name, content).subscribe();
+      // assert
+      expect(writeFileSpy).toHaveBeenCalled();
+      expect(writeFileSpy).toHaveBeenCalledWith(path, contentAsString);
+    });
   });
 });
