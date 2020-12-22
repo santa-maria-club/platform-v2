3aad07b38633a0514ff15aba9075319d29a72187
Daniel Marin <danielarturomt@gmail.com>
2020-12-22 11:31:38 -0300

    feat: generate a graph controller

--- apps/irrigation-api/src/app/graph/graph.module.ts
+++ apps/irrigation-api/src/app/graph/graph.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common'
 
 import { GraphService } from './graph.service'
+import { GraphController } from './graph.controller'
 
 @Module({
   providers: [GraphService],
+  controllers: [GraphController],
 })
 export class GraphModule {}
