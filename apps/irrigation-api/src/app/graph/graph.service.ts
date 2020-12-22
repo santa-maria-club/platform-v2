f21d46691797f574dc65012509880598983ce010
Daniel Marin <danielarturomt@gmail.com>
2020-12-22 12:00:38 -0300

    feat: add a create method in graph service

--- apps/irrigation-api/src/app/graph/graph.service.ts
+++ apps/irrigation-api/src/app/graph/graph.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { promises } from 'fs';
+
+import { CreateGraphDto } from '@platform/shared/utils/irrigation-api-interfaces';
+import { createGraphFromCreateGraphDto } from '@platform/shared/utils/irrigation-creators';
 
 @Injectable()
-export class GraphService {}
+export class GraphService {
+  async create(createGraphDto: CreateGraphDto) {
+    try {
+      const location = `${__dirname}/assets/graphs/${createGraphDto.name}.json`;
+      const graph = createGraphFromCreateGraphDto(createGraphDto, { location });
+      await promises.writeFile(
+        `${__dirname}/assets/graphs/${graph.name}.json`,
+        JSON.stringify(graph),
+      );
+      return graph;
+    } catch (err) {
+      throw Error('Error creating a graph');
+    }
+  }
+}
