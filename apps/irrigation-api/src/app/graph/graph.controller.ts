6b2717c418c6adf494aee747220869088231c40c
Daniel Marin <danielarturomt@gmail.com>
2020-12-22 12:01:50 -0300

    feat: add a create graph endpoint

--- apps/irrigation-api/src/app/graph/graph.controller.ts
+++ apps/irrigation-api/src/app/graph/graph.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+
+import { CreateGraphDto } from '@platform/shared/utils/irrigation-api-interfaces';
+import { GraphService } from './graph.service';
 
 @Controller('graphs')
-export class GraphController {}
+export class GraphController {
+  constructor(private graphService: GraphService) {}
+
+  @Post()
+  create(@Body() createGraphDto: CreateGraphDto) {
+    return this.graphService.create(createGraphDto);
+  }
+}
