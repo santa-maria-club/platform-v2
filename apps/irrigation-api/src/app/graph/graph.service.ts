c1ec5c1116118b2765937aa494562c07c3fba778
Daniel Marin <danielarturomt@gmail.com>
2020-12-24 08:38:00 -0300

    Rename graph endpoint (#14) (closes #11)
    * feat: set up a rename method in file manager
    
    * feat: set up a rename method in graph service
    
    * feat: create a view graph method and use it in update and rename
    
    * chore: clean up and adjustments
    
    * feat: set up a rename endpoint
    
    * e2e: add call to rename in http file
    
    * fix: use the new location for update in test

--- apps/irrigation-api/src/app/graph/graph.service.ts
+++ apps/irrigation-api/src/app/graph/graph.service.ts
@@ -24,10 +24,16 @@ export interface IGraphService {
     options: GraphOptions,
   ) => Observable<Graph>;
   list: (options: GraphOptions) => Observable<Graph[]>;
+  view: (graphId: string, options: GraphOptions) => Observable<Graph>;
   update: (
     graphId: string,
     nodes: Node[],
     edges: Edge[],
+    options: GraphOptions,
+  ) => Observable<Graph>;
+  rename: (
+    oldName: string,
+    newName: string,
     options: GraphOptions,
   ) => Observable<Graph>;
 }
@@ -53,6 +59,15 @@ export class GraphServiceMock implements IGraphService {
       },
     ]);
   }
+  view(graphId: string, options: GraphOptions) {
+    return of({
+      id: graphId,
+      name: 'name-123',
+      location: `${options.rootDirectory}/assets/graphs/name-123.json`,
+      nodes: [],
+      edges: [],
+    });
+  }
   update(graphId: string, nodes: Node[], edges: Edge[], options: GraphOptions) {
     return of({
       id: graphId,
@@ -61,6 +76,9 @@ export class GraphServiceMock implements IGraphService {
       nodes,
       edges,
     });
+  }
+  rename() {
+    return of(null);
   }
 }
 
@@ -100,17 +118,43 @@ export class GraphService implements IGraphService {
       );
   }
 
-  update(graphId: string, nodes: Node[], edges: Edge[], options: GraphOptions) {
+  view(graphId: string, options: GraphOptions) {
     return this.list(options).pipe(
       map((graphs) =>
         graphs
           .filter((graph) => graph.id === graphId)
           .reduce((_, curr) => curr),
       ),
+    );
+  }
+
+  update(graphId: string, nodes: Node[], edges: Edge[], options: GraphOptions) {
+    return this.view(graphId, options).pipe(
       mergeMap((graph) => {
         const updatedGraph = { ...graph, nodes, edges };
         return this.fileManagerService
           .writeFile(graph.location, updatedGraph)
+          .pipe(map(() => updatedGraph));
+      }),
+    );
+  }
+
+  rename(graphId: string, newName: string, options: GraphOptions) {
+    const newLocation = `${options.rootDirectory}/assets/graphs/${newName}.json`;
+    return this.view(graphId, options).pipe(
+      mergeMap((graph) =>
+        this.fileManagerService
+          .rename(graph.location, newLocation)
+          .pipe(map(() => graph)),
+      ),
+      mergeMap((graph) => {
+        const updatedGraph = {
+          ...graph,
+          name: newName,
+          location: newLocation,
+        };
+        return this.fileManagerService
+          .writeFile(newLocation, updatedGraph)
           .pipe(map(() => updatedGraph));
       }),
     );
