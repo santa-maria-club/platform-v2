08b33e51a376824610817c2953db8d814491fa8e
Daniel Marin <danielarturomt@gmail.com>
2020-12-24 08:38:00 -0300

    feat: Rename graph endpoint (#14) (closes #11)
    * feat: set up a rename method in file manager
    
    * feat: set up a rename method in graph service
    
    * feat: create a view graph method and use it in update and rename
    
    * chore: clean up and adjustments
    
    * feat: set up a rename endpoint
    
    * e2e: add call to rename in http file
    
    * fix: use the new location for update in test

--- apps/irrigation-api/src/app/graph/graph.service.spec.ts
+++ apps/irrigation-api/src/app/graph/graph.service.spec.ts
@@ -1,4 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { of } from 'rxjs';
 
 import {
   SharedUtilsFileManagerServiceMock,
@@ -7,7 +8,6 @@ import {
 import type { ISharedUtilsFileManagerService } from '@platform/shared/utils/file-manager';
 import { GraphService, GRAPH_SERVICE } from './graph.service';
 import type { IGraphService } from './graph.service';
-import { of } from 'rxjs';
 
 describe('GraphService', () => {
   let service: IGraphService;
@@ -80,41 +80,70 @@ describe('GraphService', () => {
       // arrange
       const graphId = '456';
       const rootDirectory = '/home';
-      const graphs = [
-        {
-          id: '123',
-          name: 'name-123',
-          location: `${rootDirectory}/assets/graphs/name-123.json`,
-          nodes: [],
-          edges: [],
-        },
-        {
-          id: '456',
-          name: 'name-456',
-          location: `${rootDirectory}/assets/graphs/name-456.json`,
-          nodes: [{ id: '4567', kind: 'faucet', label: 'Faucet #1' }],
-          edges: [],
-        },
-      ];
-      const graph = graphs.find((graph) => graph.id === graphId);
+      const graph = {
+        id: '456',
+        name: 'name-456',
+        location: `${rootDirectory}/assets/graphs/name-456.json`,
+        nodes: [{ id: '4567', kind: 'faucet', label: 'Faucet #1' }],
+        edges: [],
+      };
       const nodes = [];
       const edges = [];
       const expectedLocation = `${rootDirectory}/assets/graphs/name-456.json`;
       const writeFileSpy = jest.spyOn(fileManagerService, 'writeFile');
-      const listSpy = jest
-        .spyOn(service, 'list')
-        .mockImplementation(() => of(graphs));
+      const viewSpy = jest
+        .spyOn(service, 'view')
+        .mockImplementation(() => of(graph));
       // act
       service.update(graphId, nodes, edges, { rootDirectory }).subscribe();
       // assert
-      expect(listSpy).toHaveBeenCalled();
-      expect(listSpy).toHaveBeenCalledWith({ rootDirectory });
+      expect(viewSpy).toHaveBeenCalled();
+      expect(viewSpy).toHaveBeenCalledWith(graphId, { rootDirectory });
       expect(writeFileSpy).toHaveBeenCalled();
       expect(writeFileSpy).toHaveBeenCalledWith(expectedLocation, {
         ...graph,
         nodes,
         edges,
       });
+    });
+  });
+
+  describe('Rename Method', () => {
+    it('should call rename with old and new location', () => {
+      // arrange
+      const graphId = '456';
+      const rootDirectory = '/home';
+      const newName = 'new';
+      const graph = {
+        id: '456',
+        name: 'name-456',
+        location: `${rootDirectory}/assets/graphs/name-456.json`,
+        nodes: [{ id: '4567', kind: 'faucet', label: 'Faucet #1' }],
+        edges: [],
+      };
+      const updatedGraph = {
+        id: '456',
+        name: newName,
+        location: `${rootDirectory}/assets/graphs/${newName}.json`,
+        nodes: [{ id: '4567', kind: 'faucet', label: 'Faucet #1' }],
+        edges: [],
+      };
+      const oldLocation = `${rootDirectory}/assets/graphs/name-456.json`;
+      const newLocation = `${rootDirectory}/assets/graphs/${newName}.json`;
+      const renameSpy = jest.spyOn(fileManagerService, 'rename');
+      const writeFileSpy = jest.spyOn(fileManagerService, 'writeFile');
+      const viewSpy = jest
+        .spyOn(service, 'view')
+        .mockImplementation(() => of(graph));
+      // act
+      service.rename(graphId, newName, { rootDirectory }).subscribe();
+      // assert
+      expect(viewSpy).toHaveBeenCalled();
+      expect(viewSpy).toHaveBeenCalledWith(graphId, { rootDirectory });
+      expect(renameSpy).toHaveBeenCalled();
+      expect(renameSpy).toHaveBeenCalledWith(oldLocation, newLocation);
+      expect(writeFileSpy).toHaveBeenCalled();
+      expect(writeFileSpy).toHaveBeenCalledWith(newLocation, updatedGraph);
     });
   });
 });
